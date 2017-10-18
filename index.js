ebd0361fc3cad1150c7a0e53a40ba794b71fbad5
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 04:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function currentLine(array){
   if(array.length == 0){
     return "The line is currently empty.";
   }
-  const strn = "The line is currently: ";
+  var strn = "The line is currently: ";
   for(let i in array){
     strn = i+". "+ array[i]+", ";
   }
