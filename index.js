20e8e652f790d5ad84e6f16cb93a403fb5c09c92
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 04:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 
-//currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-//
-// nowServing(katzDeliLine); // "Currently serving Ada."
-//
-// currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
-//
+currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+
+nowServing(katzDeliLine); // "Currently serving Ada."
+
+currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+
 // takeANumber(katzDeliLine, "Matz"); // "3"
 //
 // currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
@@ -22,4 +22,12 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
   var str = "Welcome, "+ katzDeliLine[(katzDeliLine.length)-1] +". You are number "+ katzDeliLine.length +" in line."
   return str;
+}
+
+function nowServing(array){
+  if(array.isEmpty()){
+    return "There is nobody waiting to be served!";
+  }
+  
+  
 }
