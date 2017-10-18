39e5bcdc2ff06c3aec5e84ddb81e257255fb3fbd
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 04:28:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-
-nowServing(katzDeliLine); // "Currently serving Ada."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
-
-takeANumber(katzDeliLine, "Matz"); // "3"
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
-
-nowServing(katzDeliLine); // "Currently serving Grace."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
+//currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+// 
+// nowServing(katzDeliLine); // "Currently serving Ada."
+// 
+// currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
+// 
+// takeANumber(katzDeliLine, "Matz"); // "3"
+// 
+// currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
+// 
+// nowServing(katzDeliLine); // "Currently serving Grace."
+// 
+// currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
 
 function takeANumber(katzDeliLine,name){
-  for(let i in katzDeliLine){
-    
-    katzDeli.push(name);
-    
-  }
-  return
+  katzDeli.push(name);
+  var str = "Welcome, "+ katzDeli[katzDeliLine.length] +". You are number "+ katzDeliLine.length +" in line."
+  return str;
 }
