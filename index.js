a9598782ba969273795f01995598ca324691f077
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-18 05:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ function currentLine(array){
   var strn = "The line is currently: ";
   console.log(array);
   for(var i = 0; i<array.length;i++){
+    if(i==array.length-1){
+      strn = (i+1)+". "+ array[i];
+    }
     strn = (i+1)+". "+ array[i]+", ";
       console.log((i+1)+". "+ array[i]+", ");
   }
